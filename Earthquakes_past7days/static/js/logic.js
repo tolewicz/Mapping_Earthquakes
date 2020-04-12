01516caa49ded6c3c340e7a3288c53f899ce8dc4
tolewicz <tomasz.olewicz@gmail.com>
2020-04-12 13:02:08 -0700

    renamed logicScript5 to logic

--- Earthquakes_past7days/static/js/logic.js
+++ Earthquakes_past7days/static/js/logic.js
@@ -48,32 +48,137 @@ let baseMaps = {
 
   //making baseline map
   let map = L.map('mapid', {
-	center: [43.7, -79.3],
-	zoom: 11,
+	center: [39.5, -98.5],
+  zoom: 3,
 	layers: streets //setting one base layer, otherwise it's all gray
 });
 
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/tolewicz/Mapping_Earthquakes/master/torontoNeighborhoods.json";
+let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-//Json that is loaded has arrays of district that are build from arrys of micropolygons (4 coordinate points)
-//like puzzles, 
-//so some have e.g. 90 arrays, some more complicated have 500 arrays
-//the addToMap, just add the puzzles on the map
+// Create the earthquake layer for our map. "L.layerGroup()" calls out the array of map layers
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// // This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
 
 //importing data from Json address and adding it as a layer to the map
-d3.json(torontoHoods).then(function(data){
-  console.log(data)
-  L.geoJson(data, { //adding fancy deatures to the layer
-  weight:1,
-  fillColor: "yellow",
-    onEachFeature: function(features, layer) {
-      console.log(layer.feature.properties["AREA_NAME"])
-      layer.bindPopup("<h3>" + "District Name:" + layer.feature.properties["AREA_NAME"]+  "</h3>")
-    }
 
-  }).addTo(map)
-})
+function dateConversion(binaryDate){
+  var myDate = new Date( binaryDate *1000);
+  convertedDate = myDate.toLocaleString();
+  return convertedDate
+}
+
+function styleInfo(feature) {
+  magnitude = feature.properties.mag
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: getColor(feature.properties.mag),
+    color: "#000000",
+    radius: getRadius(magnitude),
+    stroke: true,
+    weight: 0.5,
+    
+  };
+}
+
+function getColor(magnitude){
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude >2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1){ 
+    return "#d4ee00";
+  }
+  else {
+  return "#98ee00";
+  }
+};
+
+function getRadius(magnitude) {
+  
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+};
+
+
+
+function dateConversion(binaryDate){
+  var myDate = new Date( binaryDate *1000);
+  convertedDate = myDate.toLocaleString();
+  return convertedDate
+}
+
+d3.json(earthquakeData).then(function(data){
+  console.log(data)
+  
+  L.geoJson(data, {
+
+    // We turn each feature into a circleMarker on the map.
+    
+    pointToLayer: function(feature, latlng) {
+              //console.log(data);
+              var rawDate = feature.properties.time;
+              var quakePlace = feature.properties.place;
+              var cmagnitude = feature.properties.mag
+
+              console.log(dateConversion(rawDate));
+              console.log(quakePlace);
+
+              return L.circleMarker(latlng).bindPopup("<h3>" + "Location: " + quakePlace +  "</h3>" 
+              + "<hr>" +"<h3>" + "Magnitude: " + cmagnitude +  "</h3>"+ "<h3>" + "time: " + convertedDate+  "</h3>");
+            },
+        
+        //we apply the style features: circles, colorts etc
+        style: styleInfo
+        })
+        .addTo(earthquakes); //that will add above code to "overlayers" 
+
+        earthquakes.addTo(map) //that will add earthquakes to the map once its called
+        
+        var legend = L.control({position: 'bottomright'});
+
+        legend.onAdd = function () {
+          var div = L.DomUtil.create('div', 'info legend');
+          
+          //defining magnitude intervals for color ranges
+          const magnitudes = [0,1,2,3,4,5];
+           
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < magnitudes.length; i++) {
+            div.innerHTML +=
+            
+            //we use getColor() function to assign color to provided magnitude
+            '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i> ' +
+            magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+    }
+    //console.log(colors)  
+
+    return div;
+};
+
+  //adding legend to map
+    legend.addTo(map);
+
+    });
+   
+
 
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
